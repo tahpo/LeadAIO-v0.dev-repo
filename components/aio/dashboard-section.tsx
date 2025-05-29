959780983bc4e7b96876497d0b1bf9a0d457ae49
ybibi <45717750+tahpo@users.noreply.github.com>
2025-05-29 13:16:43 -0700

    Fix ranking distribution chart

--- components/aio/dashboard-section.tsx
+++ components/aio/dashboard-section.tsx
@@ -177,12 +177,12 @@ export function AIODashboard() {
       </div>
       
       {/* Bottom wave */}
-      <div className="absolute -bottom-1 left-0 right-0">
+      <div className="absolute bottom-0 left-0 right-0 transform translate-y-24">
         <svg className="w-full h-24" viewBox="0 0 1440 320" preserveAspectRatio="none">
           <path fill="#ffffff" d="M0,96L48,112C96,128,192,160,288,186.7C384,213,480,235,576,213.3C672,192,768,128,864,128C960,128,1056,192,1152,213.3C1248,235,1344,213,1392,202.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         </svg>
       </div>
-      <div className="h-24"></div>
+      <div className="h-48"></div>@@ .. @@
     </section>
   )
 }
