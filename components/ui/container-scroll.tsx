fe2bb6619896825f73edc30c9d5af2fb191d82f6
ybibi <45717750+tahpo@users.noreply.github.com>
2025-05-29 17:07:29 -0700

    Fix container scroll animation

--- components/ui/container-scroll.tsx
+++ components/ui/container-scroll.tsx
@@ -27,20 +27,20 @@ export const ContainerScroll = ({
   }, []);
 
   const scaleDimensions = () => {
-    return isMobile ? [0.7, 0.9] : [1.05, 1];
+    return isMobile ? [0.9, 1] : [1, 1];
   };
 
-  const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, 0]);
   const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
-  const translate = useTransform(scrollYProgress, [0, 1], [0, -100]);
+  const translate = useTransform(scrollYProgress, [0, 1], [0, -50]);
 
   return (
     <div
-      className="h-[40rem] md:h-[45rem] flex items-center justify-center relative p-2 md:p-8"
+      className="h-[45rem] flex items-center justify-center relative"
       ref={containerRef}
     >
       <div
-        className="py-4 md:py-8 w-full relative"
+        className="w-full relative"
         style={{
           perspective: "1000px",
         }}
@@ -83,13 +83,11 @@ export const Card = ({
       style={{
         rotateX: rotate,
         scale,
-        translateY: translate,
-        boxShadow:
-          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
+        translateY: translate
       }}
-      className="max-w-5xl mx-auto h-[30rem] md:h-[35rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl"
+      className="max-w-5xl mx-auto h-[35rem] w-full border-[1px] border-gray-800/50 p-2 md:p-6 bg-[#222222] rounded-[30px]"
     >
-      <div className="h-full w-full overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4">
+      <div className="h-full w-full overflow-hidden rounded-2xl bg-zinc-900 md:rounded-2xl">
         {children}
       </div>
     </motion.div>
