e8a6f9ff9491283b50eae584653df06680c8e53e
ybibi <45717750+tahpo@users.noreply.github.com>
2025-05-30 17:42:13 -0700

    Fix background color implementation

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -25,8 +25,7 @@ const BentoGrid = ({
 const BentoCard = ({
   name,
   className,
-  background,
-  background: backgroundClass,
+  background: backgroundClassName,
   Icon,
   description,
   href,
@@ -34,8 +33,7 @@ const BentoCard = ({
 }: {
   name: string
   className: string
-  background?: ReactNode
-  background?: string
+  background: string
   Icon: any
   description: string
   href: string
@@ -45,13 +43,12 @@ const BentoCard = ({
     key={name}
     className={cn(
       "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
-      backgroundClass || "bg-white",
+      backgroundClassName,
       "[box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
       "transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
       className,
     )}
   >
-    <div>{background}</div>
     <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
       <Icon className="h-12 w-12 origin-left transform-gpu transition-all duration-300 ease-in-out group-hover:scale-75" />
       <h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
